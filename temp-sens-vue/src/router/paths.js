825fbe559e31265dbb4cf07fdeeec0c798794c1a
HarleyBerglund <55533374+HarleyBerglund@users.noreply.github.com>
2019-10-08 16:20:09 +0200

    Router fix

---
+++ temp-sens-vue/src/router/paths.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        path: '',
+        view: 'Home'
+    },
+    {
+        path: '/ExampleComp',
+        name: 'ExampleComp',
+        view: 'ExampleComp'
+    },
+    {
+        path: '/minCardComp',
+        name: 'minCardComp',
+        view: 'minCardComp'
+    },
+    {
+        path: '/Pokedex',
+        name: 'Pokedex',
+        view: 'Pokedex'
+    },
+    {
+        path: '/TempComp',
+        name: 'TempComp',
+        view: 'TempComp'
+    }
+]
