410579571ea7ceb98965e41e4afaf3bf42a12760
HarleyBerglund <55533374+HarleyBerglund@users.noreply.github.com>
2019-10-21 16:12:16 +0200

    21/10
    Bra jobbat allihop

--- temp-sens-vue/src/components/Toolbar.vue
+++ temp-sens-vue/src/components/Toolbar.vue
@@ -10,16 +10,24 @@
       dark
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Snurrig.se</v-toolbar-title>
+      <v-toolbar-title>Snurrig.se</v-toolbar-title> 
+      <!-- genom att lägga router-links direkt under inkluderas de i snurrig.se-toolbaren-->
+      <!-- Dessa router links är "permanenta" och följer med vid sidbyte, rekommenderar att ta bort dem innan ni lämnar in uppgiften åtminstone -->
+         <router-link id= b45 to="/">Snurrig.se</router-link>
+    <!-- <router-link id= b44 to="Settings">Settings</router-link> -->
+    <v-layout row>
+      <v-flex>
+        
+        <!-- v-flex och v-layout gör att vi kan flytta på login och snurrig.se med ex. justify center -->
+    <router-link id= b44 to="Login">Login/Sign in</router-link>
+    </v-flex>
+</v-layout>
+
       <!-- En "v-spacer" fyller allt utrymme mellan två "object", det är lite lättare att använda dessa än det ni gjorde tidigare -->
       <v-spacer/>
       <v-app-bar-nav-icon to='/settings'><v-icon>mdi-settings</v-icon></v-app-bar-nav-icon>
     </v-app-bar>
     <img srv="./assets/logo.png"> <!-- Denna logga finns ej, inget texture -->
-    <!-- Dessa router links är "permanenta" och följer med vid sidbyte, rekommenderar att ta bort dem innan ni lämnar in uppgiften åtminstone -->
-    <router-link to="/">Startsida</router-link>
-    <router-link to="settings">settings</router-link>
-    <router-link to="Login">Login</router-link>
 
     <v-navigation-drawer
       v-model="drawer"
@@ -78,7 +86,7 @@
       temporary
     ></v-navigation-drawer>
 
- <!--   <v-footer
+   <v-footer
       app
       color="blue-grey"
       class="white--text"
@@ -86,7 +94,7 @@
       <span>Vuetify</span>
       <div class="flex-grow-1"></div>
       <span>&copy; 2019</span>
-    </v-footer> -->
+    </v-footer>
   </v-app>
 </template>
 
@@ -101,3 +109,18 @@
     }),
   }
 </script>
+
+<style>
+#b44{
+  font-size: 14px !important;
+  text-decoration: underline overline;
+  text-align: center;
+  color: white;
+
+}
+#b45{
+  font-size: 20px;
+  text-decoration: underline overline;
+  color: white;
+}
+</style>
---
+++ temp-sens-vue/src/views/Startsida.vue
@@ -0,0 +1,112 @@
+
+
+<template>
+  <v-layout row>
+    <!-- xs-skärmar ser tabellen halverad, så den fyller halva sidan (6/12), medium-skärmar och uppåt också -->
+    <v-flex xs6 md6> 
+  <v-data-table
+    :headers="headers"
+    :items="rum"
+    class="elevation-1"
+
+  ></v-data-table>
+  </v-flex>
+</v-layout>
+
+</template>
+
+
+
+<!--<template>-->
+<!-- DEN HÄR SKA OCKSÅ IN, DET ÄR EN GRAF MEN TEMPLATE KLAGAR -->
+  <!-- <v-card
+    class="mt-4 mx-auto"
+    max-width="400"
+  > -->
+    <!-- <v-sheet
+      class="v-sheet--offset mx-auto"
+      color="cyan"
+      elevation="12"
+      max-width="calc(100% - 32px)"
+    > -->
+      <!-- <v-sparkline
+        :labels="labels"
+        :value="value"
+        color="white"
+        line-width="2"
+        padding="16"
+      ></v-sparkline>
+    </v-sheet> -->
+
+    <!-- <v-card-text class="pt-0">
+      <div class="title font-weight-light mb-2">User Registrations</div>
+      <div class="subheading font-weight-light grey--text">Last Campaign Performance</div>
+      <v-divider class="my-2"></v-divider>
+      <v-icon
+        class="mr-2"
+        small
+      >
+        mdi-clock
+      </v-icon>
+      <span class="caption grey--text font-weight-light">last registration 26 minutes ago</span>
+    </v-card-text>
+  </v-card>
+</template> -->
+
+
+
+
+<script>
+  export default {
+    data () {
+      return {
+     
+       headers: [
+          {
+            // kolumnen för rummen, vertikalt
+            text: 'Rum',
+            align: 'left',
+            value: 'name',
+          },
+          // rubriker fär resterande vertikala kolumner samt values som bestämmer i vilken kolumn värdena ska in (no sht)
+          { text: 'Förrigår', value: 'förrigår' },
+          { text: 'Igår', value: 'igår' },
+          { text: 'Idag', value: 'idag' },
+          { text: '1v', value: 'vecka'},
+        ],
+        rum: [
+          {
+            name: 'Terrariet',
+            förrigår: 40.9,
+            igår: 42.8,
+            idag: 38.4,
+          },
+          {
+            name: 'Klassrummet',
+            förrigår: 18.2,
+            igår: 17.1,
+            idag: 19.5,
+          },
+          {
+            name: 'Vardagsrummet',
+            förrigår: 21.3,
+            igår: 20.5,
+            idag: 21.2,
+          },
+          {
+            name: 'Hallonrummet',
+            förrigår: 19.5,
+            igår: 18.9,
+            idag: 19.2,
+          },
+          {
+            name: 'Pingisrummet',
+            förrigår: 35.6,
+            igår: 19.2,
+            idag: 34.8,
+          },
+        ],
+      }
+    },
+  }
+</script>
